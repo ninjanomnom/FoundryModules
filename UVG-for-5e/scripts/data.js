142ec2100e1f802ef18dd3adc1d4726dbfc180b3
Emmett Gaines <ninjanomnom@gmail.com>
2022-03-16 09:54:24 -0400

    Initial commit

---
+++ UVG-for-5e/scripts/data.js
@@ -0,0 +1,48 @@
+import { DND5E } from '../../../systems/dnd5e/module/config.js';
+
+DND5E.toolProficiencies["eng"] = "Engineer's Tools";
+DND5E.toolIds["eng"] = "UVG.ToolIdEngineer"
+
+DND5E.languages = {
+    highcommon: "UVG.LanguagesHighCommon",
+    vulgcommon: "UVG.LanguagesVulgarCommon",
+    purpspeech: "UVG.LanguagesPurpleSpeech",
+    bluenttalk: "UVG.LanguagesBluenttalk",
+    greenspeak: "UVG.LanguagesGreenspeak",
+    emercommon: "UVG.LanguagesEmeraldCommon",
+    decpolitic: "UVG.LanguagesDecapolitical",
+    saffranian: "UVG.LanguagesSaffranian",
+    caravanian: "UVG.LanguagesCaravanian",
+    oranjetic: "UVG.LanguagesOranjetic",
+    freecircle: "UVG.LanguagesFreeCircleKriol",
+    redlancant: "UVG.LanguagesRedlandDistrictCant",
+    redtongue: "UVG.LanguagesRedTongue",
+    winerspeak: "UVG.LanguagesWinerian",
+    volkantalk: "UVG.LanguagesVolkan",
+    woodlander: "UVG.LanguagesWoodlander",
+    steppetalk: "UVG.LanguagesSteppeSpeeches",
+    sunsettish: "UVG.LanguagesSunsettish",
+    wlinespeak: "UVG.LanguagesWhiteLine",
+    satrapcant: "UVG.LanguagesSatrapCanto",
+    bcityalpha: "UVG.LanguagesBlackCityAlphabet",
+    catthought: "UVG.LanguagesCatThought",
+    deepdwarvn: "UVG.LanguagesDeepDwarven",
+    bluetongue: "UVG.LanguagesBlueTongue",
+    elvenspeak: "UVG.LanguagesElven",
+    moonlander: "UVG.LanguagesMoonlander",
+    marmottalk: "UVG.LanguagesMarmotsk",
+    umberspeak: "UVG.LanguagesUmber",
+    lingspeech: "UVG.LanguagesLingish",
+    greatspeak: "UVG.LanguagesGreatLanguage",
+    trilingish: "UVG.LanguagesTrilignic",
+    vomespeech: "UVG.LanguagesVomish",
+    whitepicto: "UVG.LanguagesWhiteCityPictographic",
+    ultraspeak: "UVG.LanguagesHighUltra"
+};
+
+DND5E.skills = { }
+
+DND5E.CHARACTER_EXP_LEVELS = [
+    0, 300, 750, 1500, 3000, 6000, 12500, 25000, 50000, 99999,
+    1000000, 1000001, 1000002, 1000003, 1000004, 1000005, 1000006, 1000007, 1000008, 1000009
+];
